f932466f74bb4d4192e9ae8d87b376cf59e71824
Khai Bui <khaivubui@gmail.com>
2017-10-20 09:57:04 -0700

    Write partition

--- 04 Quick Sort/Khai_Bui/lib/quick_sort.rb
+++ 04 Quick Sort/Khai_Bui/lib/quick_sort.rb
@@ -13,9 +13,26 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    
+    prc ||= Proc.new { |x, y| x <=> y }
+
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+    pivot = array[start]
+    new_pivot_index = start
+
+    (start + 1..start + length - 1).each do |i|
+      if prc.call(pivot, array[i]) == 1
+        array[new_pivot_index + 1], array[i] =
+          array[i], array[new_pivot_index + 1]
+        new_pivot_index += 1
+      end
+    end
+
+    array[new_pivot_index], array[start] =
+      array[start], array[new_pivot_index]
+
+    new_pivot_index
   end
 end
