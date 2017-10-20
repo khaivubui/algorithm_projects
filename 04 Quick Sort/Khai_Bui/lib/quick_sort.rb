b2b68c132180a5f75f562a21abd8025aaf5abe0b
Khai Bui <khaivubui@gmail.com>
2017-10-20 09:29:34 -0700

    Write quicksort1

--- 04 Quick Sort/Khai_Bui/lib/quick_sort.rb
+++ 04 Quick Sort/Khai_Bui/lib/quick_sort.rb
@@ -4,10 +4,16 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length <= 1
+    pivot = array.first
+    left = array[1..-1].select { |num| num < pivot }
+    right = array[1..-1].reject { |num| num < pivot }
+    self.sort1(left) + [pivot] + self.sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    
   end
 
   def self.partition(array, start, length, &prc)
