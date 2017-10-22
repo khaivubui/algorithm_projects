cd856f35aadf02562666306b611bf230494be4c3
Khai Bui <khaivubui@gmail.com>
2017-10-22 16:25:44 -0700

    Write insert using some meta programming

--- 05 Binary Search Tree/Khai_Bui/lib/binary_search_tree.rb
+++ 05 Binary Search Tree/Khai_Bui/lib/binary_search_tree.rb
@@ -11,6 +11,23 @@ class BinarySearchTree
   end
 
   def insert(value)
+    return @root = BSTNode.new(value) unless @root
+
+    current_node = @root
+    while current_node
+      parent_node = current_node
+
+      case value <=> parent_node.value
+      when -1, 0
+        current_node = parent_node.left
+        parent_path = 'left'
+      when 1
+        current_node = parent_node.right
+        parent_path = 'right'
+      end
+    end
+
+    parent_node.send("#{parent_path}=", BSTNode.new(value))
   end
 
   def find(value, tree_node = @root)
