23305051e7859196fdde07417dce245b0a3838ac
Khai Bui <khaivubui@gmail.com>
2017-10-22 18:22:12 -0700

    Finish assignment

--- 05 Binary Search Tree/Khai_Bui/lib/bst_practical.rb
+++ 05 Binary Search Tree/Khai_Bui/lib/bst_practical.rb
@@ -2,4 +2,6 @@ require 'binary_search_tree'
 
 def kth_largest(tree_node, k)
   tree = tree_node.tree
+  kth_largest_value = tree.in_order_traversal(tree_node)[-k]
+  tree.find(kth_largest_value)
 end
