1e10fe57f7d12e10ad95e741eb5156893b95eb79
Khai Bui <khaivubui@gmail.com>
2017-10-22 18:03:17 -0700

    Clean up code a little

--- 05 Binary Search Tree/Khai_Bui/lib/binary_search_tree.rb
+++ 05 Binary Search Tree/Khai_Bui/lib/binary_search_tree.rb
@@ -96,8 +96,10 @@ class BinarySearchTree
 
   def is_balanced?(tree_node = @root)
     return true if depth(tree_node) <= 1
+
     (depth(tree_node.left) - depth(tree_node.right)).abs <= 1 &&
-    is_balanced?(tree_node.left) && is_balanced?(tree_node.right)
+      is_balanced?(tree_node.left) &&
+      is_balanced?(tree_node.right)
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
