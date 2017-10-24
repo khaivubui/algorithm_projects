0e082952ea2d018fe6517e46ddd2215388c29e07
Khai Bui <khaivubui@gmail.com>
2017-10-24 00:10:27 -0700

    Write basic version of topological_sort

--- 06 Graphs and Topological Sort/Khai_Bui/lib/topological_sort.rb
+++ 06 Graphs and Topological Sort/Khai_Bui/lib/topological_sort.rb
@@ -3,5 +3,24 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  sorted = []
+  top = []
 
+  vertices.each do |vertex|
+    top << vertex if vertex.in_edges.empty?
+  end
+
+  until top.empty?
+    current = top.shift
+    sorted << current
+
+    current.out_edges.each do |edge|
+      destination = edge.to_vertex
+
+      top << destination if destination.in_edges.length == 1
+      edge.destroy!
+    end
+  end
+
+  sorted
 end
