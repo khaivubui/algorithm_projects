3bd1ac40cfcc43fa68aa8687fa2176c3bf0b791f
Khai Bui <khaivubui@gmail.com>
2017-10-25 10:43:09 -0700

    Finish Blair nums

--- 07 Dynamic Programming/Khai_Bui/lib/dynamic_programming.rb
+++ 07 Dynamic Programming/Khai_Bui/lib/dynamic_programming.rb
@@ -1,11 +1,23 @@
 class DynamicProgramming
 
   def initialize
-
+    @blair_cache = {
+      0 => 0,
+      1 => 1,
+      2 => 2
+    }
   end
 
   def blair_nums(n)
+    return @blair_cache[n] if @blair_cache[n]
 
+    prev_blair = @blair_cache[n - 1] || blair_nums(n - 1)
+    prev_prev_blair = @blair_cache[n - 2] || blair_nums(n - 2)
+
+    result = prev_blair + prev_prev_blair + n * 2 - 3
+
+    @blair_cache[n] = result
+    result
   end
 
   def frog_hops_bottom_up(n)
